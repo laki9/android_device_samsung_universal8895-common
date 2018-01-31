be1e65928687b93868a207befdd6f3f252c05bf4
Ivan Meler <i_ivan@windowslive.com>
2018-01-31 16:36:52 +0100

    universal8895: Add permissions for samsung specific sensors like HRM
    Signed-off-by: Albe96XDA <albe96xda@gmail.com>

--- universal8895-common.mk
+++ universal8895-common.mk
@@ -76,7 +76,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.xml:system/etc/permissions/android.software.sip.xml \
     frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml \
-    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    $(LOCAL_PATH)/configs/permissions/com.samsung.permission.HRM_EXT.xml:system/etc/permissions/com.samsung.permission.HRM_EXT.xml \
+   $(LOCAL_PATH)/configs/permissions/com.samsung.permission.SSENSOR.xml:system/etc/permissions/com.samsung.permission.SSENSOR.xml
 
 # Graphics
 PRODUCT_AAPT_CONFIG := xlarge large
