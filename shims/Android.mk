87a7c3c1623005bf5aa376ee6e6f5e7972418d1f
Albe96XDA <albe96xda@gmail.com>
2018-10-13 12:30:18 +0530

    universal8895: remove unnecessary shims
    Signed-off-by: Albe96XDA <albe96xda@gmail.com>

--- shims/Android.mk
+++ shims/Android.mk
@@ -17,15 +17,3 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
-
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := process_info.c
-LOCAL_MODULE := libprocname
-LOCAL_MODULE_TAGS := optional
-include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := get_process_name.cpp
-LOCAL_MODULE := process_name
-LOCAL_MODULE_TAGS := optional
-include $(BUILD_SHARED_LIBRARY)
